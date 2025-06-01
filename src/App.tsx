ce52cf305aff8332c890f132a4c26ffdccfbcd3a
MohamadSalman11 <hamudesalman69@gmail.com>
2025-06-01 13:09:42 +0200

    create custom select

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { LuMonitor } from 'react-icons/lu';
 import styled from 'styled-components';
+import Select from './components/Select';
 import Header from './features/editor/Header';
 import Panel from './features/editor/Panel';
 import Sidebar from './features/editor/Sidebar';
@@ -8,16 +10,40 @@ const EditorPage = styled.div`
   width: 100%;
   height: 100vh;
   display: grid;
-  grid-template-columns: 10rem 1fr 3.5fr 1fr;
+  grid-template-columns: 10rem 34rem 3.5fr 34rem;
   grid-template-rows: 8rem 1fr;
 
   div:nth-child(4) {
     background-color: transparent;
   }
+`;
 
-  div:nth-child(5) {
-    border-left: 1px solid var(--color-gray-dark-2);
-    background-color: var(--color-black-light-2);
+const PanelContainer = styled.div`
+  border-left: 1px solid var(--color-gray-dark-2);
+  background-color: var(--color-black-light-2);
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    border-bottom: var(--border-base);
+    padding: 3.2rem 2.4rem;
+    width: 100%;
+
+    span {
+      margin-bottom: 1.6rem;
+    }
+  }
+`;
+
+const SelectorContainer = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 1.2rem;
+  font-size: 1.2rem;
+
+  svg {
+    color: var(--color-white);
+    font-size: 2.4rem;
   }
 `;
 
@@ -30,7 +56,15 @@ function App() {
         <Header />
         <Panel />
         <div></div>
-        <div></div>
+        <PanelContainer>
+          <div>
+            <span>Selector</span>
+            <SelectorContainer>
+              <LuMonitor />
+              <Select options={['Chocolate', 'Vanilla', 'Strawberry']} />
+            </SelectorContainer>
+          </div>
+        </PanelContainer>
       </EditorPage>
     </>
   );
---
+++ src/components/Select.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useRef, useState } from 'react';
+import { LuChevronDown } from 'react-icons/lu';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const Trigger = styled.button`
+  padding: 0.4rem 1.2rem;
+  background: var(--color-black-light-2);
+  border: var(--border-base);
+  border-radius: var(--border-radius-sm);
+  color: var(--color-white);
+  cursor: pointer;
+  width: 100%;
+  text-align: left;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`;
+
+const List = styled.ul<{ open: boolean }>`
+  display: ${({ open }) => (open ? 'block' : 'none')};
+  position: absolute;
+  width: 100%;
+  margin-top: 0.4rem;
+  background: var(--color-black-light-2);
+  border: var(--border-base);
+  border-radius: var(--border-radius-sm);
+  list-style: none;
+  z-index: var(--zindex-base);
+`;
+
+const Item = styled.li`
+  padding: 0.8rem 1.2rem;
+  cursor: pointer;
+
+  &:hover {
+    background: var(--color-gray-dark-2);
+  }
+`;
+
+const Select = ({ options }: { options: string[] }) => {
+  const [selected, setSelected] = useState<string>();
+  const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const onSelect = (opt: string) => {
+    setSelected(opt);
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const onClickOutside = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) setOpen(false);
+    };
+    document.addEventListener('mousedown', onClickOutside);
+    return () => document.removeEventListener('mousedown', onClickOutside);
+  }, []);
+
+  return (
+    <Wrapper ref={ref}>
+      <Trigger onClick={() => setOpen(!open)}>
+        {selected || 'Select...'}
+        <LuChevronDown />
+      </Trigger>
+      <List open={open}>
+        {options.map((opt) => (
+          <Item key={opt} onClick={() => onSelect(opt)} tabIndex={0}>
+            {opt}
+          </Item>
+        ))}
+      </List>
+    </Wrapper>
+  );
+};
+
+export default Select;

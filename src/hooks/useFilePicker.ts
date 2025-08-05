6a0dae0c4cdf2b0f3121976565402cfd44d3c614
MohamadSalman11 <hamudesalman69@gmail.com>
2025-08-05 22:14:51 +0200

    refactor useFilePicker hook

--- src/hooks/useFilePicker.ts
+++ src/hooks/useFilePicker.ts
@@ -1,11 +1,11 @@
 import { ElementsName } from '@shared/constants';
 import type { InputChangeEvent } from '@shared/typing';
-import { createElement, useRef } from 'react';
+import { createElement, useMemo, useRef } from 'react';
 
-type Options = {
+interface Options {
   accept?: string;
   onSelect: (file: File) => void;
-};
+}
 
 export const useFilePicker = ({ accept = '*', onSelect }: Options) => {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -14,17 +14,21 @@ export const useFilePicker = ({ accept = '*', onSelect }: Options) => {
     inputRef.current?.click();
   };
 
-  const input = createElement(ElementsName.Input, {
-    type: 'file',
-    accept,
-    ref: inputRef,
-    style: { display: 'none' },
-    onChange: (event: InputChangeEvent) => {
-      const file = event.target.files?.[0];
-      event.target.value = '';
-      if (file) onSelect(file);
-    }
-  });
+  const input = useMemo(
+    () =>
+      createElement(ElementsName.Input, {
+        type: 'file',
+        accept,
+        ref: inputRef,
+        style: { display: 'none' },
+        onChange: (event: InputChangeEvent) => {
+          const file = event.target.files?.[0];
+          event.target.value = '';
+          if (file) onSelect(file);
+        }
+      }),
+    [accept, onSelect]
+  );
 
   return { input, openFilePicker };
 };

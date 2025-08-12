c520ab86602650078e31cf6cea9f9f865c730490
MohamadSalman11 <hamudesalman69@gmail.com>
2025-08-13 01:35:20 +0200

    refactor: change IframeContext from type to interface

--- src/context/IframeContext.ts
+++ src/context/IframeContext.ts
@@ -1,10 +1,10 @@
 import { createContext, useContext, type RefObject } from 'react';
 import type { useIframeConnection } from '../features/editor/hooks/useIframeConnection';
 
-type IframeContext = {
+interface IframeContext {
   iframeRef: RefObject<HTMLIFrameElement | null>;
   iframeConnection: ReturnType<typeof useIframeConnection>;
-};
+}
 
 export const IframeContext = createContext<IframeContext | null>(null);
 

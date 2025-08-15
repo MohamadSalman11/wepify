7e2db0a89628de6c8235665cb6fff233aba96a7b
MohamadSalman11 <hamudesalman69@gmail.com>
2025-08-15 17:13:18 +0200

    feat(ui): add home-loading-screen

---
+++ src/init-home-loading-screen.ts
@@ -0,0 +1,39 @@
+// Constants
+const HOME_PATH = '/';
+const MAX_TIMEOUT = 10_000;
+const MIN_DISPLAY_TIME = 2000;
+const SELECTOR_HOME_LOADING_SCREEN = '.home-loading-screen';
+
+export const initHomeLoadingScreen = () => {
+  if (globalThis.location.pathname !== HOME_PATH) return;
+
+  const bodyEl = document.body;
+  const htmlEl = document.documentElement;
+  const loader = document.querySelector(SELECTOR_HOME_LOADING_SCREEN) as HTMLElement | null;
+
+  if (!loader) return;
+
+  loader.classList.add('active');
+
+  const startTime = performance.now();
+
+  bodyEl.style.overflow = 'hidden';
+
+  const hideLoader = () => {
+    loader.style.display = 'none';
+    bodyEl.style.overflow = '';
+    bodyEl.style.paddingRight = '';
+    htmlEl.style.scrollbarGutter = '';
+  };
+
+  const maxTimeout = setTimeout(hideLoader, MAX_TIMEOUT);
+
+  window.addEventListener('load', () => {
+    clearTimeout(maxTimeout);
+
+    const loadTime = performance.now() - startTime;
+    const delay = loadTime < MIN_DISPLAY_TIME ? MIN_DISPLAY_TIME : 0;
+
+    setTimeout(hideLoader, delay);
+  });
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
+import { initHomeLoadingScreen } from './init-home-loading-screen.ts';
 import store from './store.ts';
+
+initHomeLoadingScreen();
 
 const SELECTOR_ROOT_ELEMENT = '#root';
 

01b87edbf145f4780c7e06e98f17f9c06e41a5a2
MohamadSalman11 <hamudesalman69@gmail.com>
2025-08-19 16:01:18 +0200

    fix: sync new columns with app

--- iframe/ts/utils/adjustGridColumnsIfNeeded.ts
+++ iframe/ts/utils/adjustGridColumnsIfNeeded.ts
@@ -1,12 +1,21 @@
+import { MessageFromIframe } from '@shared/typing';
+import { maybeWrapWithBreakpoint } from './maybeWrapWithBreakpoint';
 import { parseGridRepeat } from './parseGridRepeat';
+import { postMessageToApp } from './postMessageToApp';
 
-export const adjustGridColumnsIfNeeded = (gridElement: HTMLElement) => {
+export const adjustGridColumnsIfNeeded = (gridElement: HTMLDivElement) => {
   const { size, count: columns } = parseGridRepeat(gridElement.style.gridTemplateColumns);
   const { count: rows } = parseGridRepeat(gridElement.style.gridTemplateRows);
   const maxItems = columns * rows;
   const childrenCount = gridElement.children.length;
+  const newColumns = columns + 1;
 
   if (childrenCount > maxItems) {
-    gridElement.style.gridTemplateColumns = `repeat(${columns + 1}, ${size === 'auto' ? '1fr' : `${size}px`})`;
+    gridElement.style.gridTemplateColumns = `repeat(${newColumns}, ${size === 'auto' ? '1fr' : `${size}px`})`;
+
+    postMessageToApp({
+      type: MessageFromIframe.ElementUpdated,
+      payload: { id: gridElement.id, fields: { ...maybeWrapWithBreakpoint({ columns: newColumns }) } }
+    });
   }
 };

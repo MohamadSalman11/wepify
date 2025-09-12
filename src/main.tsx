579db9469e4747131e43f2c01cd792de90a4452c
MohamadSalman11 <hamudesalman69@gmail.com>
2025-09-12 19:37:51 +0200

    refactor: rename initHomeLoadingScreen file name

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
-import { initHomeLoadingScreen } from './init-home-loading-screen.ts';
+import { initHomeLoadingScreen } from './initHomeLoadingScreen.ts';
 import store from './store.ts';
 
 initHomeLoadingScreen();

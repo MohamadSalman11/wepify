f0233355fdd1364862e0bf4a94d76ea89594a5ff
MohamadSalman11 <hamudesalman69@gmail.com>
2025-09-12 21:52:18 +0200

    refactor: remove unused constants

--- iframe/ts/constants.ts
+++ iframe/ts/constants.ts
@@ -1,14 +1,2 @@
 export const SELECTOR_SECTION = 'section';
 export const SELECTOR_ROOT = '#iframe-root';
-
-export const CSS_SIZES = {
-  fill: '100%',
-  screen: '100vh',
-  auto: 'fit-content'
-} as const;
-
-export const CSS_SIZES_NAME = {
-  '100%': 'fill',
-  '100vh': 'screen',
-  'fit-content': 'auto'
-} as const;
